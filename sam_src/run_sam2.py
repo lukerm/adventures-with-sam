9ab6c9c542c71f55d1d06dd3cd719a5b4654e498
lukerm <13883308+lukerm@users.noreply.github.com>
2024-12-20 07:37:32 +0000

    add website to license text

--- sam_src/run_sam2.py
+++ sam_src/run_sam2.py
@@ -1,4 +1,4 @@
-#  Copyright (C) 2024 lukerm
+#  Copyright (C) 2024 lukerm of www.zl-labs.tech
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
